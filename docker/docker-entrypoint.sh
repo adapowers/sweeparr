2ad819b345c1f6b8edaaa8f627671c4b666d32b2
Ada <ada@powe.rs>
2024-06-30 15:22:10 -0400

    make entrypoint for PUID/PGID

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set PUID and PGID if provided
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+
+# Create group if it doesn't exist
+if ! getent group sweeparr >/dev/null 2>&1; then
+    addgroup -g "$PGID" sweeparr
+fi
+
+# Create user if it doesn't exist
+if ! getent passwd sweeparr >/dev/null 2>&1; then
+    adduser -D -H -u "$PUID" -G sweeparr sweeparr
+fi
+
+# Set ownership of the shared volume directory
+chown -R sweeparr:sweeparr /shared/scripts
+
+# Execute the passed command
+exec "$@"
