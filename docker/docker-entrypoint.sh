9532b757e830c38b295c8eb34230c08d308f0832
Ada <ada@powe.rs>
2024-06-30 17:44:48 -0400

    copy from tmp

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -14,6 +14,9 @@ if ! getent passwd sweeparr >/dev/null 2>&1; then
     adduser -D -H -u "$PUID" -G sweeparr sweeparr
 fi
 
+# Copy the script from the temporary location to the shared volume directory
+cp /tmp/sweeparr/sweeparr.sh /sweeparr/sweeparr.sh
+
 # Set ownership of the shared volume directory
 chown -R sweeparr:sweeparr /sweeparr
 
